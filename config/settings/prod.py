3a01918a9c8f40482c568ac9df07bc12cde86656
miu1217 <hyun671217@daum.net>
2025-06-09 11:43:24 +0900

    black 정렬

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,11 +8,11 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
-        "ENGINE":   os.getenv("DB_ENGINE"),
-        "NAME":     os.getenv("DB_NAME"),
-        "USER":     os.getenv("DB_USER"),
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST":     os.getenv("DB_HOST"),
-        "PORT":     os.getenv("DB_PORT"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse
 from django.urls import include, path
 
 urlpatterns = [
-    path('', lambda request: HttpResponse('Hello, World!'), name='home'),
-
-    path('api/tests/', include('cognitives.urls')),
-    path('api/cognitive-tests/', include('cognitive_statistics.urls')),
+    path("", lambda request: HttpResponse("Hello, World!"), name="home"),
+    path("api/tests/", include("cognitives.urls")),
+    path("api/cognitive-tests/", include("cognitive_statistics.urls")),
 ]
