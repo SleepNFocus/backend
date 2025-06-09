2ebb57244acf6ca70282cf59610963e8219bd140
miu1217 <hyun671217@daum.net>
2025-06-09 11:49:38 +0900

    Mypy 수정

--- cognitive_statistics/views.py
+++ cognitive_statistics/views.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,47 +7,47 @@ from rest_framework.views import APIView
 class CognitiveTestTypeListAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def get(self, request: Request) -> Response:
         return Response("REQ-011")
 
 
 class CognitiveTestFormatListAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def get(self, request: Request) -> Response:
         return Response("REQ-012")
 
 
 class CognitiveTestTimeSaveAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self, request):
+    def post(self, request: Request) -> Response:
         return Response("REQ-014")
 
 
 class CognitiveTestTimeGuideAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def get(self, request: Request) -> Response:
         return Response("REQ-015")
 
 
 class CognitiveTestResultBasicAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def get(self, request: Request) -> Response:
         return Response("REQ-016")
 
 
 class CognitiveTestResultCorrelationAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def get(self, request: Request) -> Response:
         return Response("REQ-017")
 
 
 class CognitiveTestResultVisualizationAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def get(self, request: Request) -> Response:
         return Response("REQ-018")
--- cognitives/views.py
+++ cognitives/views.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,19 +7,19 @@ from rest_framework.views import APIView
 class TestPlayListAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def get(self, request: Request) -> Response:
         return Response("REQ-PLAY-LIST")
 
 
 class TestPlayAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, test_type):
+    def get(self, request: Request, test_type: str) -> Response:
         return Response(f"REQ-PLAY-{test_type.upper()}")
 
 
 class TestSubmitAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, test_type):
+    def post(self, request: Request, test_type: str) -> Response:
         return Response(f"REQ-SUBMIT-{test_type.upper()}")
