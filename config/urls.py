a5987f249a3efc451bb95d3134c352e778e64956
hyunyoung97 <gusdud04164@gmail.com>
2025-06-09 13:53:37 +0900

    admin 패스 추가

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
 
 urlpatterns = [
     path("", lambda request: HttpResponse("Hello, World!"), name="home"),
+    path("admin/", admin.site.urls),
 ]
