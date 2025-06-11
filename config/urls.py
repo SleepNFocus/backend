84670b4ecc88d999711bfcfb2bb4e9c1d208fb9f
hyunyoung97 <gusdud04164@gmail.com>
2025-06-11 12:21:07 +0900

    isort, black 오류 해결

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
+from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import include, path
-from django.contrib import admin
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from config.health_check import HealthCheckView
