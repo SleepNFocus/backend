3b8bce0eb5abb8b61dfd40ffec214a3d7f76db09
hyunyoung97 <gusdud04164@gmail.com>
2025-06-11 12:19:30 +0900

    ruff 오류 해결

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,6 +1,7 @@
+from datetime import timedelta
 from .base import *  # noqa
 
-DEBUG = True  # 디버그 모드(개발 모드) 에러가 발생 하면 장고에서 노란 화면으로 알려줌
+DEBUG = True  # 디버그 모드(개발 모드) 에러가 발생하면 장고에서 노란 화면으로 알려줌
 
 ALLOWED_HOSTS = ["*"]
 
