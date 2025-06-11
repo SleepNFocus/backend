4c609cddb6575c30e5158b70f0f25a1c9a2c82a9
hyunyoung97 <gusdud04164@gmail.com>
2025-06-11 12:00:11 +0900

    dev랑 충돌 해결

--- config/urls.py
+++ config/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path("api/users/", include("users.urls")),
     path("api/tests/", include("cognitives.urls")),
     path("api/cognitive-tests/", include("cognitive_statistics.urls")),
+    path("health/", HealthCheckView.as_view(), name="health-check"),
+    # 🔐 JWT 토큰 발급/갱신 엔드포인트
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # 각 앱 API 연결
+    path("api/sleepRecord/", include("sleep_record.urls")),
 ]
