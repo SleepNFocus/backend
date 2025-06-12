16ce49ea582cac536280d0906959369d2aa7039e
1yul2 <hanyul0417@gmail.com>
2025-06-12 14:32:39 +0900

    병합은 언제쯤..

--- cognitive_statistics/views.py
+++ cognitive_statistics/views.py
@@ -4,11 +4,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import (
-    CognitiveTestFormat,
-    CognitiveTestResult,
-    CognitiveTestType,
-)
+from .models import CognitiveTestFormat, CognitiveTestResult, CognitiveTestType
 from .serializers import (
     CognitiveTestFormatSerializer,
     CognitiveTestResultSerializer,
