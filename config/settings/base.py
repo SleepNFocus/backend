adf4e36c2e5845adfe0164cfe8c3f55f763324e7
hyunyoung97 <gusdud04164@gmail.com>
2025-06-17 17:35:02 +0900

    feat: 환경변수 .env 연동 및 python-dotenv 적용

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,6 +24,7 @@ ALLOWED_HOSTS = ["*"]
 AUTH_USER_MODEL = "users.User"
 
 
+load_dotenv()
 KAKAO_CLIENT_ID = os.getenv("KAKAO_CLIENT_ID")
 
 
