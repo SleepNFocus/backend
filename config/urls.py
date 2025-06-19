e8d663e0908cea1b708ad0495a34fba6bf087f54
hyunyoung97 <gusdud04164@gmail.com>
2025-06-19 16:50:51 +0900

    개발 환경에서 media 파일 서빙을 위한 static URL 설정 추가

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 # 작성자: 한율
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import include, path
@@ -72,3 +73,6 @@ if settings.DEBUG:
         ),
         path("sentry-debug/", trigger_error, name="sentry-debug"),
     ]
+
+# 개발 환경에서 MEDIA 파일(media/*) 접근 가능하도록 URL 패턴 추가
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
