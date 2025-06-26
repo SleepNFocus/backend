f93abf8e82e89758b7abe0ef7b1a4fa666aa9db8
workface <with.you70@gmail.com>
2025-06-26 13:29:28 +0900

    black, isort, ruff

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,7 +5,7 @@ from .base import *  # noqa
 DEBUG = True  # 디버그 모드(개발 모드) 에러가 발생 하면 장고에서 노란 화면으로 알려줌
 ALLOWED_HOSTS = [
     "localhost",
-    "127.0.0.1",      # 도커 실행 시
+    "127.0.0.1",  # 도커 실행 시
     "www.dev.focusz.site",
     "dev.focusz.site",
     "www.focusz.site",
